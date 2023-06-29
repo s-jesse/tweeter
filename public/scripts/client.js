aaf3ce4d88680f187a8451f71624da684a3c6be5
Jesse Sandhu <s.jesse23@gmail.com>
2023-06-29 07:39:18 -0700

    edit files to remove dead code / unneeded comments and format files to make more readable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,36 +1,33 @@
-// how does event arg work????
 $("#target").on("submit", function(event) {
   event.preventDefault();
 
   
-  
+  // validition error conditionals
+
   let textLength = $('#tweet-text').val().length;
-  console.log("tweetLength: ", textLength)
   
   if (textLength === 0) {
-    $(".hidden-error .tweet-err").css("display", "inline-block").slideDown("slow")
+    $(".hidden-error .tweet-err").css("display", "inline-block").slideDown("slow");
   } else if (textLength >= 0) {
     $(".hidden-error .tweet-err").css("display", "none");
   }
 
 
+// create ajax get/ post  and load tweets
 
   let serializeData = $(this).serialize();
 
   $.ajax("/tweets", { method: 'POST', data: serializeData })
     .then(function(data) {
-      console.log('Success: ', data);
-      loadTweets()
-      $('#tweet-text').val('')
-
-      //$button.replaceWith(morePostsHtml);
+      loadTweets();
+      $('#tweet-text').val('');
     });
 });
+
 
 const loadTweets = function() {
   $.ajax("/tweets", { method: 'GET' })
     .then(function(data) {
-      console.log("data", data)
       renderTweets(data);
     });
 };
@@ -38,64 +35,32 @@ const loadTweets = function() {
 
 loadTweets();
 
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 // escape function
 
-const escape = function (str) {
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-//const safeHTML = `<p>${escape(textFromUser)}</p>`;
+// icon stored variables
+
 const iconFlag = (`<i class="fa-solid fa-flag"></i>`);
-const iconRetweet =(`<i class="fa-solid fa-retweet"></i>`);
+const iconRetweet = (`<i class="fa-solid fa-retweet"></i>`);
 const iconHeart = (`<i class="fa-solid fa-heart"></i>`);  
+
+// function to create html page dynamically
 
 const createTweetElement = function(tweetObj) {
   const $tweet = (`<article class="tweet-article">
     <header class="tweet-header">
-    <h4><img class="avatar"src = "${escape(tweetObj.user.avatars)}">${escape(tweetObj.user.name)}</h4>
-      <h4>${tweetObj.user.handle}</h4>
-      </header>
-  <p class="tweet-body">${escape(tweetObj.content.text)}</p>
-  <footer class="tweet-footer">${timeago.format(tweetObj["created_at"])} <div class="icons">${iconFlag}${iconRetweet}${iconHeart}</div></footer>
-  </article>`);
-
-  // const $tweet = $(`<article class="tweet-body">${tweetObj.content.text}</article>`);
+      <h4><img class="avatar"src = "${(tweetObj.user.avatars)}">${(tweetObj.user.name)}</h4>
+        <h4>${tweetObj.user.handle}</h4>
+          </header>
+            <p class="tweet-body">${escape(tweetObj.content.text)}</p>
+              <footer class="tweet-footer">${timeago.format(tweetObj["created_at"])} 
+                <div class="icons">${iconFlag}${iconRetweet}${iconHeart}</div></footer>
+                  </article>`);
 
 
   let tweetElement = $('.tweets-container').prepend($tweet);
@@ -104,15 +69,8 @@ const createTweetElement = function(tweetObj) {
 };
 
 const renderTweets = function(data) {
-  // let tweetInfo = data.content.text;
   for (let contentVal in data) {
     createTweetElement(data[contentVal]);
-    console.log("contentVal:", data[contentVal]);
   }
 };
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-// }
 
-//renderTweets(tweetData);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,42 +1,13 @@
-console.log("loaded")
-
-
-// $("p").click(function() {
-//   alert("The paragraph was clicked.");
-// });
-// let count = 10;
-
-// $("#tweet-text").on('keydown', function() {
-//   console.log("this:", this); //The this keyword is a reference to the button
-//   // alert("was pressed.");
-//   $("#tweet-text").on('keypress', function() {
-//     console.log("this:", this); //The this keyword is a reference to the button
-//     if ("keypress" < count) {
-//         alert("was pressed.");
-
-//     }
-// });
-// });
-
 $('#tweet-text').on('input', function() {
-let textLength = $('#tweet-text').val().length;
-let maxLength = 140;
-let tweetLength = $( this ).val().length;
-$('.counter').text(maxLength - tweetLength)
-if (tweetLength > 140) {
-  $( ".hidden-error .char-err" ).css("display", "inline-block").slideDown("slow")
-  $( "div" ).find( ".counter" ).css( "background-color", "red" );
-} 
-if (tweetLength < 141) {
-  $( ".hidden-error .char-err" ).css("display", "none")
-  $( "div" ).find( ".counter" ).css( "background-color", "#f4f1ec" );
-}
-
+  let maxLength = 140;
+  let tweetLength = $(this).val().length;
+  $('.counter').text(maxLength - tweetLength);
+  if (tweetLength > 140) {
+    $(".hidden-error .char-err").css("display", "inline-block").slideDown("slow");
+    $("div").find(".counter").css("background-color", "red");
+  }
+  if (tweetLength < 141) {
+    $(".hidden-error .char-err").css("display", "none");
+    $("div").find(".counter").css("background-color", "#f4f1ec");
+  }
 });
-
-
-
-// $( "#tweet-text" ).on( "keydown", function() {
-//   $( "counter" ).trigger( "keypress" );
-//   console.log("counter triggered:", trigger)
-// } );
