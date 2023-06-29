f9d8a7a38c10027dc175fe242c778035f9a03ba2
Jesse Sandhu <s.jesse23@gmail.com>
2023-06-29 06:08:57 -0700

    additional validition erros added with jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -23,7 +23,14 @@ let textLength = $('#tweet-text').val().length;
 let maxLength = 140;
 let tweetLength = $( this ).val().length;
 $('.counter').text(maxLength - tweetLength)
-
+if (tweetLength > 140) {
+  $( ".hidden-error .char-err" ).css("display", "inline-block").slideDown("slow")
+  $( "div" ).find( ".counter" ).css( "background-color", "red" );
+} 
+if (tweetLength < 141) {
+  $( ".hidden-error .char-err" ).css("display", "none")
+  $( "div" ).find( ".counter" ).css( "background-color", "#f4f1ec" );
+}
 
 });
 
